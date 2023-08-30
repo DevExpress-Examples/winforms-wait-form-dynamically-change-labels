a0c0b75a89db55c2a545cd2b7e80f3c229e361b8
DevExpressExampleBot <fakeEmail>
2023-08-30 13:48:18 +0400

    autogenerate VB

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,34 +1,30 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraSplashScreen
 Imports System.Threading
 
 Namespace WaitForm_SetDescription
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub btnShowWaitForm_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnShowWaitForm.Click
-			'Open Wait Form
-			SplashScreenManager.ShowForm(Me, GetType(WaitForm1), True, True, False)
+    Public Partial Class Form1
+        Inherits Form
 
-			'The Wait Form is opened in a separate thread. To change its Description, use the SetWaitFormDescription method.
-			For i As Integer = 1 To 100
-				SplashScreenManager.Default.SetWaitFormDescription(i.ToString() & "%")
-				Thread.Sleep(25)
-			Next i
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			'Close Wait Form
-			SplashScreenManager.CloseForm(False)
-		End Sub
-	End Class
+        Private Sub btnShowWaitForm_Click(ByVal sender As Object, ByVal e As EventArgs)
+            'Open Wait Form
+            SplashScreenManager.ShowForm(Me, GetType(WaitForm1), True, True, False)
+            'The Wait Form is opened in a separate thread. To change its Description, use the SetWaitFormDescription method.
+            For i As Integer = 1 To 100
+                Call SplashScreenManager.Default.SetWaitFormDescription(i.ToString() & "%")
+                Thread.Sleep(25)
+            Next
+
+            'Close Wait Form
+            SplashScreenManager.CloseForm(False)
+        End Sub
+    End Class
 End Namespace
--- VB/WaitForm1.vb
+++ VB/WaitForm1.vb
@@ -1,39 +1,35 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
-Imports System.Windows.Forms
 Imports DevExpress.XtraWaitForm
 
 Namespace WaitForm_SetDescription
-	Partial Public Class WaitForm1
-		Inherits WaitForm
-		Public Sub New()
-			InitializeComponent()
-			Me.progressPanel1.AutoSize = True
-		End Sub
 
-		#Region "Overrides"
+    Public Partial Class WaitForm1
+        Inherits WaitForm
 
-		Public Overrides Sub SetCaption(ByVal caption As String)
-			MyBase.SetCaption(caption)
-			Me.progressPanel1.Caption = caption
-		End Sub
-		Public Overrides Sub SetDescription(ByVal description As String)
-			MyBase.SetDescription(description)
-			Me.progressPanel1.Description = description
-		End Sub
-		Public Overrides Sub ProcessCommand(ByVal cmd As System.Enum, ByVal arg As Object)
-			MyBase.ProcessCommand(cmd, arg)
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            progressPanel1.AutoSize = True
+        End Sub
 
-		#End Region
+'#Region "Overrides"
+        Public Overrides Sub SetCaption(ByVal caption As String)
+            MyBase.SetCaption(caption)
+            progressPanel1.Caption = caption
+        End Sub
 
-		Public Enum WaitFormCommand
-                                Command1
-		End Enum
-	End Class
+        Public Overrides Sub SetDescription(ByVal description As String)
+            MyBase.SetDescription(description)
+            progressPanel1.Description = description
+        End Sub
+
+        Public Overrides Sub ProcessCommand(ByVal cmd As [Enum], ByVal arg As Object)
+            MyBase.ProcessCommand(cmd, arg)
+        End Sub
+
+'#End Region
+        Public Enum WaitFormCommand
+        End Enum
+    End Class
 End Namespace
